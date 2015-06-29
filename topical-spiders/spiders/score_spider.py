644fd7b6a714fa21812158806281feef9d656003
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-06-29 11:43:38 +0500

    Removed unused.

--- topical-spiders/spiders/score_spider.py
+++ topical-spiders/spiders/score_spider.py
@@ -2,19 +2,15 @@ from scrapy.spider import Spider
 from scrapy.http import Request
 from scrapy.exceptions import DontCloseSpider
 from scrapy import signals
-from content_processor import ContentProcessor
-from classifier import TopicClassifier
+from fronteracrawler.classifier.content_processor import ContentProcessor
 
 
 class ScoreSpider(Spider):
     name = 'score'
 
-    def __init__(self, topic_dict, features=None, disable_classifier=False, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ScoreSpider, self).__init__(*args, **kwargs)
         self.contentprocessor = ContentProcessor()
-        self.featuresoutput = open(features, 'w') if features else None
-        self.topicclassifier = TopicClassifier(topic_dict)
-        self.disable_classifier = disable_classifier
 
     # stable branch
     def set_crawler(self, crawler):
@@ -27,23 +23,11 @@ class ScoreSpider(Spider):
 
     def make_requests_from_url(self, url):
         r = super(ScoreSpider, self).make_requests_from_url(url)
-        return self.process_request(r)
-
-    def process_request(self, request):
-        request.meta['score'] = 1.0
-        return request
+        return r
 
     def parse(self, response):
         pc = self.contentprocessor.process_response(response)
-        if self.featuresoutput:
-            obj_str = pc.to_JSON()
-            self.featuresoutput.write(str(len(obj_str)))
-            self.featuresoutput.write('\n')
-            self.featuresoutput.write(obj_str)
-            self.featuresoutput.write('\n')
-
-        if self.disable_classifier or self.topicclassifier.classify_paragraphs(pc.paragraphs):
-            for link in pc.links:
-                r = Request(url=link.url)
-                r.meta.update(link_text=link.text)
-                yield r
+        for link in pc.links:
+            r = Request(url=link.url)
+            r.meta.update(link_text=link.text)
+            yield r
